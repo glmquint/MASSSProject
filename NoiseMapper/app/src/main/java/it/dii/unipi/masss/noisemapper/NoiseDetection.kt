453f31bdf5cd655105824e66eae87ee205a07bc1
Giacomo Volpi <giacomo.volpi@icloud.com>
2024-04-20 16:37:19 +0200

    Add stop button and refactor UI of noise sensing activity

--- NoiseMapper/app/src/main/java/it/dii/unipi/masss/noisemapper/NoiseDetection.kt
+++ NoiseMapper/app/src/main/java/it/dii/unipi/masss/noisemapper/NoiseDetection.kt
@@ -1,10 +1,12 @@
 package it.dii.unipi.masss.noisemapper
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.media.MediaRecorder
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -18,15 +20,24 @@ import java.util.TimerTask
 
 class NoiseDetection : AppCompatActivity() {
     private val RECORD_AUDIO_PERMISSION_REQUEST_CODE = 101
+    private val REFRESH_RATE = 500
     private var mRecorder : MediaRecorder? = null
+    private val timer = Timer()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.noise_detection)
-        // You can initialize views or perform any other necessary setup here
-        // request user permission for micrpohone
+        val button: Button = findViewById(R.id.stop_button)
         requestPermission()
-    }
+        button.setOnClickListener {
+            // Create an Intent to return to the main activity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
 
+            // Close the current activity
+            onDestroy()
+        }
+    }
+    
     private fun requestPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
             != PackageManager.PERMISSION_GRANTED
@@ -55,7 +66,7 @@ class NoiseDetection : AppCompatActivity() {
                     Log.d("MicrophoneRequest", "Permission granted")
                     noise_sampling()
                 } else {
-                    val resultTextView: TextView = findViewById(R.id.noiseLevel)
+                    val resultTextView: TextView = findViewById(R.id.upper_text_noise_activity)
                     resultTextView.text = getString(R.string.microphone_request_not_granted)
                 }
             }
@@ -67,8 +78,7 @@ class NoiseDetection : AppCompatActivity() {
         mRecorder!!.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
         mRecorder!!.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
         mRecorder!!.setOutputFile(FileOutputStream(File(cacheDir, "audio.mp3")).fd)
-        val timer = Timer()
-        timer.scheduleAtFixedRate(RecorderTask(mRecorder!!), 0, 700)
+        timer.scheduleAtFixedRate(RecorderTask(mRecorder!!), 0, REFRESH_RATE.toLong())
         try {
             mRecorder!!.prepare()
             mRecorder!!.start()
@@ -79,16 +89,34 @@ class NoiseDetection : AppCompatActivity() {
         }
     }
     private inner class RecorderTask(private val recorder: MediaRecorder) : TimerTask() {
-        private val sound = findViewById<TextView>(R.id.noiseLevel)
+        private val sound = findViewById<TextView>(R.id.db_level)
 
         override fun run() {
             runOnUiThread {
                 val amplitude = recorder.maxAmplitude
                 val amplitudeDb = 20 * Math.log10(Math.abs(amplitude).toDouble())
                 Log.i("NoiseDetection", "Level db is $amplitudeDb")
-                sound.text = "DB level is" + amplitudeDb
+                sound.text = String.format("%.1f", amplitudeDb)
             }
         }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        mRecorder?.stop()
+        mRecorder?.release()
+        timer.cancel()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mRecorder?.stop()
+        mRecorder?.release()
+        timer.cancel()
+    }
+    override fun onResume() {
+        super.onResume()
+        noise_sampling()
+
     }
 
 }
