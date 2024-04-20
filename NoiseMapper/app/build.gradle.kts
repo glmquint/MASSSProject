ab0ffe5d3f07a28fe225cc07765db58b94133455
Giacomo Volpi <giacomo.volpi@icloud.com>
2024-04-20 10:26:15 +0200

    Change default UI to Material UI

--- NoiseMapper/app/build.gradle.kts
+++ NoiseMapper/app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
