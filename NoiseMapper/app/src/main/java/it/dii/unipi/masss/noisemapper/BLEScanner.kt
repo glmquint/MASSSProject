b21fe9eabcfd9755ee045a21ce45918f52ea767c
Guillaume Quint <glmquint@gmail.com>
2024-04-25 18:17:13 +0200

    working simple ble listener

--- NoiseMapper/app/src/main/java/it/dii/unipi/masss/noisemapper/BLEScanner.kt
+++ NoiseMapper/app/src/main/java/it/dii/unipi/masss/noisemapper/BLEScanner.kt
@@ -1,22 +1,31 @@
 package it.dii.unipi.masss.noisemapper
 
 import android.content.pm.PackageManager
+import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.kontakt.sdk.android.ble.device.BeaconRegion
 import com.kontakt.sdk.android.ble.manager.ProximityManagerFactory
 import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleIBeaconListener
 import com.kontakt.sdk.android.common.profile.IBeaconDevice
 import com.kontakt.sdk.android.common.profile.IBeaconRegion
+import java.util.Collections
+import java.util.UUID
 
 class BLEScanner (activity: BLEScannerActivity) {
-    private val BLUETOOTH_SCAN_PERMISSION_REQUEST_CODE = 102
     private val proximityManager = ProximityManagerFactory.create(activity)
 
     init {
+        setupProximityManager(activity)
+        //setupSpaces()
+    }
+
+    fun setupProximityManager(activity: BLEScannerActivity){
         proximityManager.setIBeaconListener(object : SimpleIBeaconListener() {
             override fun onIBeaconDiscovered(device: IBeaconDevice, region: IBeaconRegion) {
                 // print the discovered iBeacon device
                 println("iBeacon: Discovered iBeacon device: $device")
+                activity.findViewById<TextView>(R.id.textView2).text = "new device discovered: ${device.uniqueId}"
             }
 
             override fun onIBeaconLost(device: IBeaconDevice, region: IBeaconRegion) {
@@ -27,9 +36,29 @@ class BLEScanner (activity: BLEScannerActivity) {
             override fun onIBeaconsUpdated(beacons: MutableList<IBeaconDevice>, region: IBeaconRegion) {
                 // print the updated iBeacon devices
                 println("iBeacon: Updated iBeacon devices: $beacons")
+                // concatenate the unique IDs of the updated iBeacon devices
+
             }
         })
     }
+
+    /*
+    fun setupSpaces()
+    {
+        //Setting up single iBeacon region. Put your own desired values here.
+        val region : IBeaconRegion = BeaconRegion.Builder()
+            .identifier("My Region") //Region identifier is mandatory.
+            .proximity(UUID.fromString("f7826da6-4fa2-4e98-8024-bc5b71e0893e")) //Default Kontakt.io proximity.
+            //Optional major and minor values
+            //.major(1)
+            //.minor(1)
+            .build();
+
+        proximityManager.spaces().iBeaconRegion(region)
+            .forceResolveRegions(Collections.singleton(UUID.fromString("f7826da6-4fa2-4e98-8024-bc5b71e0893e")));
+    }
+
+     */
 
 
     fun stopScanning() {
--- NoiseMapper/app/src/main/java/it/dii/unipi/masss/noisemapper/BLEScannerActivity.kt
+++ NoiseMapper/app/src/main/java/it/dii/unipi/masss/noisemapper/BLEScannerActivity.kt
@@ -1,20 +1,23 @@
 package it.dii.unipi.masss.noisemapper
 
+import android.bluetooth.BluetoothManager
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.kontakt.sdk.android.common.KontaktSDK
 
 class BLEScannerActivity : AppCompatActivity() {
+    private val BLUETOOTH_SCAN_PERMISSION_REQUEST_CODE = 102
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ble_layout)
         KontaktSDK.initialize(this);
-        var ble_scanner = BLEScanner(this)
         val button: Button = findViewById(R.id.stop_ble)
         button.setOnClickListener {
             // Create an Intent to return to the main activity
@@ -24,27 +27,54 @@ class BLEScannerActivity : AppCompatActivity() {
             // Close the current activity
             onStop()
         }
-        //requestPermissions()
+        requestPermissions()
+        val ble_scanner = BLEScanner(this)
         ble_scanner.startScanning()
     }
 
-    /*
     private fun requestPermissions() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH)
-            != PackageManager.PERMISSION_GRANTED
+        val selfPermission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+        if (selfPermission != PackageManager.PERMISSION_GRANTED
         ) {
             // Permission is not granted, request the permission
             ActivityCompat.requestPermissions(
                 this,
-                arrayOf(Manifest.permission.BLUETOOTH),
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
                 BLUETOOTH_SCAN_PERMISSION_REQUEST_CODE
             )
         } else {
             println("iBeacon: Starting scanning")
 
         }
+        // check that bluetooth is enabled, if not, ask the user to enable it
+        val bluetoothAdapter = android.bluetooth.BluetoothManager::class.java.cast(
+            getSystemService(android.content.Context.BLUETOOTH_SERVICE)
+        )?.adapter
+        if (!(bluetoothAdapter?.isEnabled)!!) {
+            val enableBtIntent = Intent(android.bluetooth.BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivity(enableBtIntent)
+        }
     }
-     */
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.i("BluetoothRequest", "Request code is $requestCode")
+        when (requestCode) {
+            BLUETOOTH_SCAN_PERMISSION_REQUEST_CODE -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    Log.d("BluetoothRequest", "Permission granted")
+
+                } else {
+                    Log.d("iBeacon",  "Permission not granted")
+                }
+            }
+        }
+    }
 
     override fun onStart() {
         super.onStart()
