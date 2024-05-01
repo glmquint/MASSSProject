277c541d1f0882875cc06ad1604cecb399e20aec
Giacomo Volpi <giacomo.volpi@icloud.com>
2024-05-01 12:02:23 +0200

    Update db structure and create REST apis

--- NoiseMapper/server/server.py
+++ NoiseMapper/server/server.py
@@ -1,32 +1,31 @@
 from flask import Flask,request, send_from_directory
 import sqlite3
 app = Flask(__name__)
-
-conn = sqlite3.connect('../db/mapdata.db', check_same_thread=False)
+conn = sqlite3.connect('db/mapdata.db', check_same_thread=False)
 c = conn.cursor()
 
 # Create a table if it doesn't exist
-c.execute('''CREATE TABLE IF NOT EXISTS requests
-             (id INTEGER PRIMARY KEY AUTOINCREMENT, data TEXT)''')
+c.execute('''CREATE TABLE IF NOT EXISTS noise_measurements
+             (timestamp INTEGER NOT NULL,beacon_id TEXT, distance REAL, noise REAL)''')
 conn.commit()
 
-@app.route('/save_request', methods=['POST'])
+@app.route('/measurements', methods=['POST'])
 def save_request():
-    data = request.json.get('data')
+    data = request.json
     if data:
         # Save the request data to the database
-        c.execute("INSERT INTO requests (data) VALUES (?)", (data,))
+        c.execute("INSERT INTO noise_measurements (timestamp, beacon_id, distance, noise) VALUES (?,?,?,?)", (data['timestamp'], data['beacon_id'], data['distance'], data['noise']))
         conn.commit()
         return 'Request saved successfully'
     else:
         return 'No data provided'
 
-@app.route('/get_requests')
+@app.route('/measurements', methods=['GET'])
 def get_requests():
     # Retrieve all requests from the database
-    c.execute("SELECT * FROM requests")
+    c.execute("SELECT * FROM noise_measurements ORDER BY timestamp DESC")
     rows = c.fetchall()
-    return {'requests': rows}
+    return rows
 
 @app.route('/resources/<filename>')
 def download_file(filename):
