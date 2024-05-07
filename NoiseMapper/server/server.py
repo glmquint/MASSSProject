b6655e39a6801e5c9cd71ed73bc31077842e87a5
Giacomo Volpi <giacomo.volpi@icloud.com>
2024-05-07 15:49:35 +0200

    Multiple insert on db

--- NoiseMapper/server/server.py
+++ NoiseMapper/server/server.py
@@ -12,14 +12,23 @@ conn.commit()
 
 @app.route('/measurements', methods=['POST'])
 def save_request():
+    # get the data from the request and save to file
     data = request.json
     if data:
-        # Save the request data to the database
-        c.execute("INSERT INTO noise_measurements (timestamp, room, noise) VALUES (?,?,?)", (int(time()), data['room'], data['noise']))
-        conn.commit()
-        return 'Request saved successfully'
-    else:
-        return 'No data provided'
+        # Check if data is a list
+        if isinstance(data, list):
+            # Prepare a list to hold the tuples for insertion
+            measurements = []
+            for measurement in data:
+                # Each measurement should be a dictionary with the keys 'timestamp', 'room', and 'noise'
+                if 'timestamp' in measurement and 'room' in measurement and 'noise' in measurement:
+                    measurements.append((measurement['timestamp'], measurement['room'], measurement['noise']))
+            # Use executemany to insert all the measurements at once
+            c.executemany("INSERT INTO noise_measurements (timestamp, room, noise) VALUES (?,?,?)", measurements)
+            conn.commit()
+            return 'Requests saved successfully'
+        else:
+            return 'Data provided is not a list'
 
 @app.route('/measurements', methods=['GET'])
 def get_requests():
@@ -50,4 +59,4 @@ def download_file(filename):
     except Exception as e:
         return e
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5002)
+    app.run(debug=True,host='0.0.0.0',port=5002)
