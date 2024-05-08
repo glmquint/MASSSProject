3cc20e0b9795f1db233ed62f4d02969e30c123f7
Fabio <fabizen99@gmail.com>
2024-05-08 12:52:10 +0200

    enable debug mode in server

--- NoiseMapper/server/server.py
+++ NoiseMapper/server/server.py
@@ -53,4 +53,4 @@ def download_file(filename):
     except Exception as e:
         return e
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5002)
+    app.run(host='0.0.0.0',port=5002, debug=True)
