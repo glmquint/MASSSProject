46a5ce429326317d24aa616abdea4d69d65cdaeb
Giacomo Volpi <giacomo.volpi99@gmail.com>
2024-05-15 10:55:09 +0200

    Add lock on microphone to avoid illegalstateexception for MediaRecorder

--- NoiseMapper/app/src/main/java/it/dii/unipi/masss/noisemapper/NoiseActivity.kt
+++ NoiseMapper/app/src/main/java/it/dii/unipi/masss/noisemapper/NoiseActivity.kt
@@ -41,25 +41,27 @@ class NoiseActivity: AppCompatActivity() {
     private lateinit var updateMapTimer: Timer
     lateinit var bleConfig: BLEConfig
     private lateinit var noise_map_io: NoiseMapIO
-    private lateinit var notGrantedPermissions : Array<String>
+    private lateinit var notGrantedPermissions: Array<String>
     private val requiredPermissions = arrayOf(Manifest.permission.RECORD_AUDIO) +
-                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S)
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
-                else arrayOf(
-                    Manifest.permission.BLUETOOTH_SCAN,
-                    Manifest.permission.BLUETOOTH_CONNECT,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ) // Note that there is no need to ask about ACCESS_FINE_LOCATION anymore for BT scanning purposes for VERSION_CODES.S and higher if you add android:usesPermissionFlags="neverForLocation" under BLUETOOTH_SCAN in your manifest file.
-    private var bluetoothAdapter : BluetoothAdapter? = null
-    var startDate:Long = 0
-    var endDate:Long = 0
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S)
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+            else arrayOf(
+                Manifest.permission.BLUETOOTH_SCAN,
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) // Note that there is no need to ask about ACCESS_FINE_LOCATION anymore for BT scanning purposes for VERSION_CODES.S and higher if you add android:usesPermissionFlags="neverForLocation" under BLUETOOTH_SCAN in your manifest file.
+    private var bluetoothAdapter: BluetoothAdapter? = null
+    var startDate: Long = 0
+    var endDate: Long = 0
+    private var microphone_on_lock: Any = Any()
+    private var mic_is_on: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.noise_activity)
         Log.i("NoiseActivity", "Noise activity started")
         bleConfig = BLEConfig(this)
-        if (!bleConfig.gotConfig()){
+        if (!bleConfig.gotConfig()) {
             Toast.makeText(
                 this,
                 "Please connect to Internet for the first app lunch",
@@ -74,7 +76,8 @@ class NoiseActivity: AppCompatActivity() {
         bluetoothAdapter = android.bluetooth.BluetoothManager::class.java.cast(
             getSystemService(android.content.Context.BLUETOOTH_SERVICE)
         )?.adapter
-        startDate = System.currentTimeMillis()-this.resources.getInteger(R.integer.MILLISECONDS_IN_A_WEEK)
+        startDate =
+            System.currentTimeMillis() - this.resources.getInteger(R.integer.MILLISECONDS_IN_A_WEEK)
         endDate = System.currentTimeMillis()
         pickDateButton = findViewById(R.id.pick_date_button)
         pickDateButton.setOnClickListener {
@@ -103,24 +106,24 @@ class NoiseActivity: AppCompatActivity() {
 
         // register for the switch compat change event
         switchCompat.setOnCheckedChangeListener { _, isChecked ->
-            Log.i("NoiseMapper","Switch event")
-            if (isChecked){
-                Log.i("NoiseMapper","Switch is ON: entering in sensing state")
+            Log.i("NoiseMapper", "Switch event")
+            if (isChecked) {
+                Log.i("NoiseMapper", "Switch is ON: entering in sensing state")
                 enterSensingState()
-            }else{
-                Log.i("NoiseMapper","Switch is OFF: exiting sensing state")
+            } else {
+                Log.i("NoiseMapper", "Switch is OFF: exiting sensing state")
                 exitSensingState()
             }
         }
     }
 
-    fun createGraphUI(){
+    fun createGraphUI() {
         graph = Graph(filesDir.absolutePath, bleConfig)
         webView = findViewById(R.id.map_web_view)
         switchCompat = findViewById(R.id.sensing_on_off)
         // create the graph
         val timeAnHourBefore = Calendar.getInstance()
-        timeAnHourBefore.add(Calendar.HOUR_OF_DAY, -1*24*7*4) // TODO: nocheckin
+        timeAnHourBefore.add(Calendar.HOUR_OF_DAY, -1 * 24 * 7 * 4) // TODO: nocheckin
         updateMap(startTS = timeAnHourBefore.timeInMillis)
 
         webView.settings.javaScriptEnabled = true;
@@ -129,13 +132,14 @@ class NoiseActivity: AppCompatActivity() {
         //webView.webViewClient = WebViewClient()
         webView.loadUrl("file://" + filesDir.absolutePath + "/output.html")
     }
+
     private fun inSensingState(): Boolean {
         return findViewById<SwitchCompat>(R.id.sensing_on_off).isChecked
     }
 
-    fun enterSensingState(){
+    fun enterSensingState() {
         pickDateButton.text = getString(R.string.refresh_map)
-        Log.i("NoiseMapper","Entering sensing state")
+        Log.i("NoiseMapper", "Entering sensing state")
         while (!permissionCheck()) {
             requestPermissions()
         }
@@ -160,6 +164,9 @@ class NoiseActivity: AppCompatActivity() {
     private fun startSensing() {
         noise_microphone.startListening()
         ble_scanner.startScanning()
+        synchronized(microphone_on_lock) {
+            mic_is_on = true
+        }
     }
 
     inner class RecorderTask() : TimerTask() {
@@ -173,30 +180,57 @@ class NoiseActivity: AppCompatActivity() {
             this.mRecorder = mRecorder
             this.sound = sound
         }
+
         override fun run() {
             runOnUiThread {
-                val amplitude = mRecorder.maxAmplitude
-                Log.i("NoiseDetection", "Recorder max amplitude is $amplitude")
-                var amplitudeDb = 20 * log10(abs(if (amplitude==0) 1 else amplitude).toDouble())
-                /*
+                synchronized(microphone_on_lock) {
+                    if (mic_is_on) {
+
+                        val amplitude = mRecorder.maxAmplitude
+                        Log.i("NoiseDetection", "Recorder max amplitude is $amplitude")
+                        var amplitudeDb =
+                            20 * log10(abs(if (amplitude == 0) 1 else amplitude).toDouble())
+                        /*
                 if (isNearObject) {
                     amplitudeDb += DB_ADJUSTMENT_PROXIMITY_SENSOR // TODO: calibrate this value
                     Log.i("NoiseDetection", "Proximity sensor detected an object")
                 }
                 */
-                val currentTimestamp = System.currentTimeMillis()
-                map_noise_level[currentTimestamp] = amplitudeDb
-                Log.i("NoiseDetection", "Level db is $amplitudeDb at time $currentTimestamp")
-                sound.text = String.format("%.1f dB", amplitudeDb)
-                when {
-                    amplitudeDb > 80 -> { // High noise level
-                        sound.setTextColor(ContextCompat.getColor(this@NoiseActivity, R.color.high_noise))
-                    }
-                    amplitudeDb > 60 -> { // Medium noise level
-                        sound.setTextColor(ContextCompat.getColor(this@NoiseActivity, R.color.medium_noise))
-                    }
-                    else -> { // Low noise level
-                        sound.setTextColor(ContextCompat.getColor(this@NoiseActivity, R.color.low_noise))
+                        val currentTimestamp = System.currentTimeMillis()
+                        map_noise_level[currentTimestamp] = amplitudeDb
+                        Log.i(
+                            "NoiseDetection",
+                            "Level db is $amplitudeDb at time $currentTimestamp"
+                        )
+                        sound.text = String.format("%.1f dB", amplitudeDb)
+                        when {
+                            amplitudeDb > 80 -> { // High noise level
+                                sound.setTextColor(
+                                    ContextCompat.getColor(
+                                        this@NoiseActivity,
+                                        R.color.high_noise
+                                    )
+                                )
+                            }
+
+                            amplitudeDb > 60 -> { // Medium noise level
+                                sound.setTextColor(
+                                    ContextCompat.getColor(
+                                        this@NoiseActivity,
+                                        R.color.medium_noise
+                                    )
+                                )
+                            }
+
+                            else -> { // Low noise level
+                                sound.setTextColor(
+                                    ContextCompat.getColor(
+                                        this@NoiseActivity,
+                                        R.color.low_noise
+                                    )
+                                )
+                            }
+                        }
                     }
                 }
             }
@@ -208,7 +242,8 @@ class NoiseActivity: AppCompatActivity() {
             continue // HACK: if the user doesn't enable the bluetooth, the app will be stuck here forever
         }
         val recorderTask = RecorderTask()
-        noise_microphone = NoiseMicrophone(this,cacheDir, findViewById(R.id.db_level), recorderTask)
+        noise_microphone =
+            NoiseMicrophone(this, cacheDir, findViewById(R.id.db_level), recorderTask)
         ble_scanner = BLEScanner(this)
     }
 
@@ -221,7 +256,7 @@ class NoiseActivity: AppCompatActivity() {
         return notGrantedPermissions.isEmpty()
     }
 
-    private fun requestPermissions(){
+    private fun requestPermissions() {
         if (notGrantedPermissions.isEmpty()) {
             Log.w("NoiseMapper", "Permissions already granted")
             return
@@ -237,12 +272,12 @@ class NoiseActivity: AppCompatActivity() {
 
     private fun scheduleUpdate() {
         // start a timer
-        updateMapTimer = fixedRateTimer(initialDelay = 2000, period = 10*1000) {
+        updateMapTimer = fixedRateTimer(initialDelay = 2000, period = 10 * 1000) {
             updateMap()
         }
     }
 
-    private fun updateMap(startTS : Long? = null, endTS : Long? = null) {
+    private fun updateMap(startTS: Long? = null, endTS: Long? = null) {
         val currentTime = Calendar.getInstance()
 
         // Get time an hour before
@@ -250,8 +285,8 @@ class NoiseActivity: AppCompatActivity() {
         timeAnHourBefore.add(Calendar.HOUR_OF_DAY, -1)
 
         // Convert to Unix timestamp
-        val currentUnixTimestamp = endTS?:currentTime.timeInMillis
-        val previousHourUnixTimestamp = startTS?:timeAnHourBefore.timeInMillis
+        val currentUnixTimestamp = endTS ?: currentTime.timeInMillis
+        val previousHourUnixTimestamp = startTS ?: timeAnHourBefore.timeInMillis
 
         // Log the results
         Log.i("PollingRequest", "Current time: ${currentTime.time}")
@@ -262,24 +297,32 @@ class NoiseActivity: AppCompatActivity() {
             "PollingRequest",
             "Unix timestamp of time an hour before: $previousHourUnixTimestamp"
         )
-        graph.makeplot(noise_map_io.performGetRequest(previousHourUnixTimestamp / 1000, currentUnixTimestamp / 1000))
+        graph.makeplot(
+            noise_map_io.performGetRequest(
+                previousHourUnixTimestamp / 1000,
+                currentUnixTimestamp / 1000
+            )
+        )
         runOnUiThread {
             webView.loadUrl("file://" + filesDir.absolutePath + "/output.html")
         }
     }
 
-    private fun exitSensingState(){
+    private fun exitSensingState() {
         pickDateButton.text = getString(R.string.pick_date)
-        Log.i("NoiseMapper","Exiting sensing state")
+        Log.i("NoiseMapper", "Exiting sensing state")
         stopSensing()
         stopUpdate()
     }
 
     private fun stopSensing() {
+        synchronized(microphone_on_lock) {
+            mic_is_on = false
+        }
         noise_microphone.stopListening()
         ble_scanner.stopScanning()
         // reset the text value of the noise indicator
-        val sound : TextView = findViewById(R.id.db_level)
+        val sound: TextView = findViewById(R.id.db_level)
         sound.text = getString(R.string.db_level_no_sensing)
         sound.setTextColor(findViewById<TextView>(R.id.current_room).currentTextColor)
 
@@ -289,9 +332,9 @@ class NoiseActivity: AppCompatActivity() {
         updateMapTimer.cancel()
     }
 
-    override fun onStop(){
+    override fun onStop() {
         super.onStop()
-        if (inSensingState()){
+        if (inSensingState()) {
             switchCompat.isChecked = false
         }
     }
