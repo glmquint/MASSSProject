3e752660234c42817d4ebaf93effcd290f47fb82
Fabio <fabizen99@gmail.com>
2024-05-15 10:32:49 +0200

    add regex to check input

--- NoiseMapper/app/src/main/java/it/dii/unipi/masss/noisemapper/MainActivity.kt
+++ NoiseMapper/app/src/main/java/it/dii/unipi/masss/noisemapper/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(){
@@ -16,8 +17,15 @@ class MainActivity : AppCompatActivity(){
         findViewById<View>(R.id.button_noise_map).setOnClickListener {
             // get the editText text field and check if it is empty
             var text_server : String = editText.text.toString()
-            if (text_server.isEmpty()) {
+            // regex to check if the text is a valid IP address
+            val ipPattern = Regex(
+                "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}" +
+                        "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+            )
+            if (text_server.isEmpty() || !ipPattern.matches(text_server)) {
                 Log.i("MainActivity", "Empty text field")
+                // make a toast to notify the user
+                Toast.makeText(this, "Please insert a valid IP address", Toast.LENGTH_LONG).show()
                 return@setOnClickListener
             }
             else{
